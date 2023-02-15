c9a33dd59b3903efbce579426b61432c877e6811
MonikaJaraite <monjara@gmail.com>
2023-02-15 12:27:09 +0200

    Homework #1-1

---
+++ 001/hw.js
@@ -0,0 +1,27 @@
+console.log('Tikrinu');
+
+/*
+1. Programiškai ridenkite du žaidimo kauliukus- sugeneruokite du atsitiktinius skaičius nuo 1 iki 6 Jeigu kauliukų suma didesnė nei 8 jūs laimėjote, priešingu atveju pralošėte. Išveskite atsakymą, kuriame būtų abiejų kauliukų reikšmės ir išvada laimėjote ar pralošėte.
+*/
+
+function getRandom(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+const dice1 = (getRandom(1,6));
+const dice2 = (getRandom(1,6));
+
+console.log (dice1, dice2);
+
+if (dice1 + dice2 >= 8) {
+    console.log ('Laimejau!');
+} else {
+    console.log ('Pralosiau...')
+}
+
+/*
+2. Gyveno du katinukai, Pilkis ir Murklys. Jų svoriai kilogramais buvo atsitiktiniai dydžiai nuo 3 iki 6. Parašyti programą, kuri išvestų katinukų svorius ir apskaičiuotų, kuris katinukas yra lengvesnis. Atsakymas turi būti katinukų vardai su jų svoriais ir lengvesnio katinuko vardas. Jeigu katinukai sveria vienodai, vietoj katinuko vardo parašykite, kad “katinukų svoriai vienodi”.
+*/
+
