bd1508402a91b5290a2aa6ee1845e30798a3c908
MonikaJaraite <monjara@gmail.com>
2023-02-15 12:50:13 +0200

    Homework #1-2

--- 001/hw.js
+++ 001/hw.js
@@ -10,8 +10,8 @@ function getRandom(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-const dice1 = (getRandom(1,6));
-const dice2 = (getRandom(1,6));
+const dice1 = (getRandom(1, 6));
+const dice2 = (getRandom(1, 6));
 
 console.log (dice1, dice2);
 
@@ -24,4 +24,23 @@ if (dice1 + dice2 >= 8) {
 /*
 2. Gyveno du katinukai, Pilkis ir Murklys. Jų svoriai kilogramais buvo atsitiktiniai dydžiai nuo 3 iki 6. Parašyti programą, kuri išvestų katinukų svorius ir apskaičiuotų, kuris katinukas yra lengvesnis. Atsakymas turi būti katinukų vardai su jų svoriais ir lengvesnio katinuko vardas. Jeigu katinukai sveria vienodai, vietoj katinuko vardo parašykite, kad “katinukų svoriai vienodi”.
 */
+
+const Pilkis = (getRandom(3, 6));
+const Murklys = (getRandom(3, 6));
+
+console.log (Pilkis, Murklys);
+
+if (Pilkis < Murklys) {
+    console.log (
+        'Pilkis' + '=' + Pilkis +';' + 
+        ' Murklys' + '=' + Murklys +';' +        
+        ' Lengvesnis Pilkis')
+} else if (Pilkis > Murklys) {
+    console.log (
+        'Pilkis' + '=' + Pilkis +';' + 
+        ' Murklys' + '=' + Murklys +';' +        
+        ' Lengvesnis Murklys')
+} else {
+    console.log ('Katinukų svoriai vienodi');
+}
 
